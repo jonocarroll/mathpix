b4bb3a0d8ddef0798e5fbed2200e60073eece900
Jonathan Carroll <jono@jcarroll.com.au>
2017-03-14 21:56:34 +1030

    test API key usage

--- tests/testthat/test_api.R
+++ tests/testthat/test_api.R
@@ -8,3 +8,7 @@ test_that("eq1 returns correct LaTeX", {
                  "$$\n \\int \\frac { 4 x } { \\sqrt { x ^ { 2 } + 1 } } d x  \n$$")
 })
 
+test_that("Travis successfully uses API key", {
+    expect_message(mathpix(system.file("eq_no_01.png", package = "mathpix")),
+                   "Using Mathpix APP_ID=jcarroll\n")
+})
