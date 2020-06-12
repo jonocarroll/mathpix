cfeb2b5f2415fb4297592d991119cff492a97d69
Jonathan Carroll <jono@jcarroll.com.au>
2020-06-12 22:21:18 +0930

    removed deactivated key added mechanism to launch browser to obtain api key minor update of test with new api result version bump

--- tests/testthat/test_api.R
+++ tests/testthat/test_api.R
@@ -4,13 +4,13 @@ context("Connect to API")
 test_that("eq1 returns correct LaTeX", {
     skip_on_cran()
     expect_equal(mathpix(system.file("extdata", "eq_no_01.png", package = "mathpix"), insert = FALSE),
-                 "$$\n \\int \\frac { 4x } { \\sqrt { x ^ { 2} + 1} } d x \n$$")
+                 "$$\n \\int \\frac { 4 x } { \\sqrt { x ^ { 2 } + 1 } } d x \n$$")
 })
 
 test_that("Travis successfully uses API key", {
     skip_on_cran()
     expect_message(mathpix(system.file("extdata", "eq_no_01.png", package = "mathpix"), insert = FALSE),
-                   "Using Mathpix APP_ID=jcarroll\n")
+                   "Using Mathpix APP_ID = jcarroll2\n")
 })
 
 test_that("Retrying image processing can be successful", {
